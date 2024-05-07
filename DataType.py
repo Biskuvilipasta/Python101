f50aca9c656255424eaddb6d2c7a38961fc35e36
Alperen <alperen460183@gmail.com>
2024-05-07 23:53:08 +0300

    Python101

---
+++ DataType.py
@@ -0,0 +1,13 @@
+sentence = "Hello"
+
+print(str(sentence))
+
+a = 5
+b = 4
+
+print(a)
+print(int(a))
+print(str(a))
+print(bool(a))
+
+print(str(a) + str(b))
---
+++ VariableAssignment.py
@@ -0,0 +1,18 @@
+name = "Alperen Sevim"
+print(name)
+sentence = "I will learn python today."
+print(sentence)
+
+a = 1
+print(a)
+
+a = 5
+b = 4
+result = a + b
+print(result)
+print(a+b)
+
+a = "5"
+b = "4"
+print(a+b)
+
